46e77c2ca81d2a1de1c5e8b7f907830821fa3b41
Tomasz Gajewski <gajewski.tomasz2@gmail.com>
2017-09-16 12:44:41 +0000

    add replacer

---
+++ src/tools/Replacer.php
@@ -0,0 +1,39 @@
+<?php
+namespace braga\tools\tools;
+/**
+ * Created on 16.09.2017 14:39:43
+ * @author Tomasz Gajewski
+ * package frontoffice
+ * error prefix
+ */
+class Replacer
+{
+	// -------------------------------------------------------------------------
+	protected $message;
+	/**
+	 *
+	 * @var array
+	 */
+	protected $data;
+	// -------------------------------------------------------------------------
+	function __construct($message = null)
+	{
+		$this->message = $message;
+	}
+	// -------------------------------------------------------------------------
+	public function add($search, $replace)
+	{
+		$this->data[$search] = $replace;
+	}
+	// -------------------------------------------------------------------------
+	public function getFormated()
+	{
+		$retval = $this->msg;
+		foreach($this->data as $search => $replace)
+		{
+			$retval = str_replace($search, $replace, $retval);
+		}
+		return $retval;
+	}
+	// -------------------------------------------------------------------------
+}
