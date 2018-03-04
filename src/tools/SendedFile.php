cd288482a077338217e6094e2bdf9c06b9062d25
Tomasz Gajewski (JamesBlond) <9467981+tgajewski007@users.noreply.github.com>
2018-03-04 10:36:12 +0000

    NEW - bug 1383: Udostępnione naliczenie, ma być ReadOnly https://bugs.openfly.eu/show_bug.cgi?id=1383

--- src/tools/SendedFile.php
+++ src/tools/SendedFile.php
@@ -85,7 +85,7 @@ class SendedFile
 		header("Cache-Control: no-transform; max-age=0; proxy-revalidate; no-cache; must-revalidate; no-store; post-check=0; pre-check=0");
 		header("Pragma: public");
 		header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
-		header("Content-type: " . $cn);
+		header("Content-Type: " . $contentType);
 		header('Content-Length: ' . $size);
 	}
 	// -------------------------------------------------------------------------
