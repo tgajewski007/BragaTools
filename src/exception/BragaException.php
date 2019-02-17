a078e10591a24f631422a088f42a517414f833b2
Tomasz Gajewski <tgajewski007@gmail.com>
2019-02-17 19:20:28 +0100

    .

--- src/exception/BragaException.php
+++ src/exception/BragaException.php
@@ -2,9 +2,13 @@
 namespace braga\tools\exception;
 class BragaException extends \RuntimeException
 {
+	protected $previous;
 	public function __construct($message = null, $code = null, $previous = null)
 	{
 		parent::__construct($message = null, $code = null, $previous = null);
+		$this->message = $message;
+		$this->code = $code;
+		$this->previous = $previous;
 	}
 }
 
