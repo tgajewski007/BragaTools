159a6ed8b705e0018af1e2f878ca4e7b4eed582f
Tomasz Gajewski <tgajewski007@gmail.com>
2022-01-23 13:20:38 +0100

    dodanie obsługi query w path

--- src/api/RestController.php
+++ src/api/RestController.php
@@ -4,21 +4,50 @@ class RestController extends BaseRestController
 {
 	// -----------------------------------------------------------------------------------------------------------------
 	/**
+	 * @var string
+	 */
+	protected $urlPrefix = "";
+	// -----------------------------------------------------------------------------------------------------------------
+	/**
 	 * @var ApiFiltr[]
 	 */
-	protected $filtr;
+	protected $filtr = [];
+	// -----------------------------------------------------------------------------------------------------------------
+	/**
+	 * @param string $urlPrefix
+	 */
+	public function setUrlPrefix($urlPrefix)
+	{
+		$this->urlPrefix = $urlPrefix;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	public function addApiFiltr(ApiFiltr $a)
+	{
+		$this->filtr[] = $a;
+	}
 	// -----------------------------------------------------------------------------------------------------------------
 	public function doAction()
 	{
+		$tmp = parse_url($_SERVER["REQUEST_URI"]);
+		$path = isset($tmp["path"]) ? $tmp["path"] : null;
+
 		foreach($this->filtr as $f)
 		{
 			if($f->method == $_SERVER["REQUEST_METHOD"])
 			{
 				$matches = null;
-				$retval = preg_match_all($f->urlRegExp, $_SERVER["REQUEST_URI"], $matches);
+				$retval = preg_match_all($this->urlPrefix . $f->urlRegExp, $path, $matches);
 				if($retval)
 				{
 					$param = $this->cleanMatches($matches);
+
+					if(isset($tmp["query"]))
+					{
+						$paramQuery = null;
+						parse_str($tmp["query"], $paramQuery);
+						$param = array_merge($param, $paramQuery);
+					}
+
 					$f->function->call($this, $param);
 					return;
 				}
