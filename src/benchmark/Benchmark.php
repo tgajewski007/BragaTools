15c5cf403fe56ebea6bce7afea2509f2a1877066
Tomasz Gajewski <tgajewski007@gmail.com>
2022-12-29 18:55:02 +0100

    add Simplebenchmark

--- src/benchmark/Benchmark.php
+++ src/benchmark/Benchmark.php
@@ -22,13 +22,31 @@ class Benchmark
 	 */
 	protected $loggerClassNama = BaseLogger::class;
 	// -----------------------------------------------------------------------------------------------------------------
-	public function __construct($loggerClassNama = null)
+	/**
+	 * @var Benchmark
+	 */
+	private static ?Benchmark $instance;
+	// -----------------------------------------------------------------------------------------------------------------
+	private function __construct($loggerClassNama = null)
 	{
 		$this->events[] = new Item("#START", JsonSerializer::toJson(["_SERVER" => $_SERVER, "_REQUEST" => $_REQUEST]));
 		if(!empty($loggerClassNama))
 		{
 			$this->loggerClassNama = new $loggerClassNama();
 		}
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	public static function init($loggerClassNama = null)
+	{
+		if(empty(self::$instance))
+		{
+			self::$instance = new self($loggerClassNama);
+		}
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	public static function add($mark, $context)
+	{
+		self::$instance->events[] = new Item($mark, $context);
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 	public function __destruct()
