70ad6153f41000664e4d13c75769e14a222b71c7
Tomasz Gajewski <tgajewski007@gmail.com>
2022-12-30 18:47:07 +0100

    fix

--- src/benchmark/Benchmark.php
+++ src/benchmark/Benchmark.php
@@ -4,6 +4,7 @@ namespace braga\tools\benchmark;
 
 use braga\graylogger\BaseLogger;
 use braga\tools\tools\JsonSerializer;
+use Throwable;
 /**
  * Created 29.12.2022 18:37
  * error prefix
@@ -29,11 +30,18 @@ class Benchmark
 	// -----------------------------------------------------------------------------------------------------------------
 	private function __construct($loggerClassNama = null)
 	{
-		if(!empty($loggerClassNama))
+		try
 		{
-			$this->loggerClassNama = new $loggerClassNama();
+			if(!empty($loggerClassNama))
+			{
+				$this->loggerClassNama = new $loggerClassNama();
+			}
+			$this->events["#START"] = new Item("#START", JsonSerializer::toJson(["_SERVER" => $_SERVER, "_REQUEST" => $_REQUEST]));
 		}
-		$this->events["#START"] = new Item("#START", JsonSerializer::toJson(["_SERVER" => $_SERVER, "_REQUEST" => $_REQUEST]));
+		catch(Throwable $e)
+		{
+			$this->loggerClassNama::exception($e);
+		}
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 	public static function init($loggerClassNama = null)
@@ -46,23 +54,37 @@ class Benchmark
 	// -----------------------------------------------------------------------------------------------------------------
 	public static function add($mark, $context = null)
 	{
-		$index = toTag($mark) . "_" . strtoupper(getRandomStringLetterOnly(5));
-		self::$instance->events[$index] = new Item($mark, $context);
+		try
+		{
+			$index = toTag($mark) . "_" . strtoupper(getRandomStringLetterOnly(5));
+			self::$instance->events[$index] = new Item($mark, $context);
+		}
+		catch(Throwable $e)
+		{
+			self::$instance->loggerClassNama::exception($e);
+		}
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 	public function __destruct()
 	{
-		$this->events["#END"] = new Item("#END");
-		$basetime = $this->events["#START"]->timestamp;
-		foreach($this->events as $event)
+		try
 		{
-			$event->duration = $event->timestamp - $basetime;
-			$basetime = $event->timestamp;
+			$this->events["#END"] = new Item("#END");
+			$basetime = $this->events["#START"]->timestamp;
+			foreach($this->events as $event)
+			{
+				$event->duration = $event->timestamp - $basetime;
+				$basetime = $event->timestamp;
+			}
+			$context = [];
+			$context["Events"] = $this->events;
+			$context["Duration"] = $this->events["#END"]->duration;
+			$this->loggerClassNama::info("BENCHMARK: " . $this->events["#END"]->duration, $context);
 		}
-		$context = [];
-		$context["Events"] = $this->events;
-		$context["Duration"] = $this->events["#END"]->duration;
-		$this->loggerClassNama::info("BENCHMARK: " . $this->events["#END"]->duration, $context);
+		catch(Throwable $e)
+		{
+			$this->loggerClassNama::exception($e);
+		}
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 }
