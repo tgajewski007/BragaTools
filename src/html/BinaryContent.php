f8bb57f55827745d04bec0ae626e7633ad6933a1
Tomasz Gajewski <tgajewski007@gmail.com>
2023-01-06 12:26:49 +0100

    cleanup

--- src/html/BinaryContent.php
+++ src/html/BinaryContent.php
@@ -15,7 +15,7 @@ class BinaryContent
 	/**
 	 * Metoda wysyła binarną zawartość z określonym Content-Type
 	 */
-	public static function send($filename, $content, ContentType $contentType = ContentType::DOWNLOAD)
+	public static function send($filename, $content, string $contentType = "application/x-download")
 	{
 		Benchmark::add(__METHOD__);
 		self::sendHeader($filename, strlen($content), $contentType);
@@ -28,14 +28,14 @@ class BinaryContent
 	/**
 	 * Metoda wysyła nagłówki niezbędne do pobrania pliku
 	 */
-	private static function sendHeader($filename, $size, ContentType $contentType)
+	private static function sendHeader($filename, $size, $contentType)
 	{
 		Benchmark::add(__METHOD__);
 		header("Expires: " . date("c"));
 		header("Cache-Control: no-transform; max-age=0; proxy-revalidate; no-cache; must-revalidate; no-store; post-check=0; pre-check=0");
 		header("Pragma: public");
 		header("Content-Disposition: attachment; filename*=UTF-8''" . rawurlencode($filename));
-		header("Content-Type: " . $contentType->value);
+		header("Content-Type: " . $contentType);
 		header('Content-Length: ' . $size);
 		header('Connection: close');
 	}
