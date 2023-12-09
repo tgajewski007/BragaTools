4cb272b8166255253b300e327124859684728969
Tomasz Gajewski <tgajewski007@gmail.com>
2023-12-09 20:33:14 +0100

    Dodanie persysntencji i kontoli wykoanania kolejności kroków poprzez zależności i kontolę wywołania jednokrotnego eventów dla ProcessController

--- src/process/ProcessController.php
+++ src/process/ProcessController.php
@@ -2,7 +2,11 @@
 
 namespace braga\tools\process;
 
+use braga\tools\exception\BragaException;
+use braga\tools\process\exception\CantProcessEventException;
+use braga\tools\process\exception\CantProcessStepException;
 use braga\tools\process\exception\ProcessException;
+use braga\tools\tools\JsonSerializer;
 /**
  * Created 21.02.2023 18:36
  * error prefix
@@ -11,6 +15,19 @@ use braga\tools\process\exception\ProcessException;
 class ProcessController
 {
 	// -----------------------------------------------------------------------------------------------------------------
+	protected ?ProcessInstancePersistance $processInstance;
+	// -----------------------------------------------------------------------------------------------------------------
+	public function getPersistanceProcessInstance(): ProcessInstancePersistance
+	{
+		$this->processInstance->initialized = true;
+		return $this->processInstance;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	public function restorePersistanceProcessInstance(ProcessInstancePersistance $processInstance): void
+	{
+		$this->processInstance = $processInstance;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
 	/**
 	 * @var ProcessCallback[][]
 	 */
@@ -18,27 +35,108 @@ class ProcessController
 	// -----------------------------------------------------------------------------------------------------------------
 	public function addCallback(ProcessCallback $callback, string $event): void
 	{
+		if(empty($this->processInstance))
+		{
+			$this->processInstance = new ProcessInstancePersistance();
+		}
 		$this->callbackAction[$event][] = $callback;
+		if(!$this->processInstance->initialized)
+		{
+			$this->processInstance->addCallbackStep($callback);
+			$this->processInstance->addEventHandled($event);
+		}
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 	public function fireEvent(string $event, mixed ...$arg)
 	{
 		if(isset($this->callbackAction[$event]))
 		{
-			foreach($this->callbackAction[$event] as $callback)
+			try
 			{
-				ProcessLogger::debug("FireEvent: " . $event, [ "event" => $event, "classname" => get_class($callback) ]);
-				try
+				$this->canProcessEvent($event);
+				foreach($this->callbackAction[$event] as $callback)
 				{
-					$callback->call($this, ...$arg);
+					ProcessLogger::debug("FireEvent: " . $event, [ "event" => $event, "classname" => get_class($callback) ]);
+					try
+					{
+						$callback->call($this, ...$arg);
+					}
+					catch(ProcessException $e)
+					{
+						ProcessLogger::exception($e);
+						$callback->fail($e, $this, ...$arg);
+					}
+					$this->processInstance->setStepProceeed($callback);
 				}
-				catch(ProcessException $e)
+			}
+			catch(CantProcessEventException $e)
+			{
+				ProcessLogger::exception($e);
+				$callback->onCantProcessEvent($e, $this, ...$arg);
+			}
+			$this->setEventHandled($event);
+			$this->setCallbackStepProceded($callback);
+		}
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	/**
+	 * @return void
+	 * @throws CantProcessEventException|BragaException
+	 */
+	protected function canProcessEvent(string $eventName): void
+	{
+		if(!isset($this->processInstance))
+		{
+			throw new BragaException("Nie ustawiono instancji procesu");
+		}
+		if($this->processInstance->isEventHandled($eventName))
+		{
+			throw new CantProcessEventException("Zdarzenie " . $eventName . " zostało już wcześniej obsłużone");
+		}
+
+		$dependences = $this->getDependences($eventName);
+
+		$stepsProcededStatus = $this->processInstance->getStepsProcededStatus();
+		foreach($stepsProcededStatus as $className => $step)
+		{
+			$var1 = isset($dependences[$className]);
+			$var2 = $step->isProceseed();
+			if(!($var1 xor $var2))
+			{
+				ProcessLogger::error("Brak spełnienia zależności: " . $className . " => " . $step->isProceseed(), [ "dependeces" => JsonSerializer::toJson($dependences), "stepsStatus" => JsonSerializer::toJson($stepsProcededStatus) ]);
+				throw new CantProcessStepException("Krok: (" . $className . ") nie może zostać wykonany z powodu niespełnionych zależności");
+			}
+		}
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	protected function setEventHandled(string $event)
+	{
+		$this->processInstance->setEventHandled($event);
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	/**
+	 * @param string $eventName
+	 * @return array classname in key on array
+	 */
+	protected function getDependences(string $eventName): array
+	{
+		$retval = [];
+		if(isset($this->callbackAction[$eventName]))
+		{
+			foreach($this->callbackAction[$eventName] as $callback)
+			{
+				foreach($callback->getCallbackDependecy() as $callbackClassNameDependeces)
 				{
-					ProcessLogger::exception($e);
-					$callback->fail($e, $this, ...$arg);
+					$retval[$callbackClassNameDependeces] = 1;
 				}
 			}
 		}
+		return $retval;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	protected function setCallbackStepProceded(ProcessCallback $callback)
+	{
+		$this->processInstance->setStepProceeed($callback);
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 }
---
+++ src/process/exception/CantProcessEventException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace braga\tools\process\exception;
+
+use braga\tools\exception\BragaException;
+use braga\tools\process\exception\ProcessException;
+/**
+ * Created 09.12.2023 17:07
+ * error prefix
+ * @autor Tomasz Gajewski
+ */
+class CantProcessEventException extends BragaException
+{
+}
