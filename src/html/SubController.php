ac0c84275baa362a60c46bc86456531dc4b8c220
Tomasz Gajewski <tgajewski007@gmail.com>
2025-03-10 18:46:12 +0100

    dodanie ułatwień do Controllera do rejestowaia działań / akcji

--- src/html/SubController.php
+++ src/html/SubController.php
@@ -2,6 +2,7 @@
 
 namespace braga\tools\html;
 
+use braga\tools\exception\BragaException;
 use braga\tools\tools\Retval;
 /**
  * Created 10.03.2025 10:40
@@ -11,13 +12,27 @@ use braga\tools\tools\Retval;
 abstract class SubController
 {
 	// -----------------------------------------------------------------------------------------------------------------
+	protected Retval $r;
+	// ----------------------------------------------------------------------------------------------------------------
 	public function __construct(protected Controler $parentController)
 	{
+		$this->r = $this->parentController->r;
 	}
 	// -----------------------------------------------------------------------------------------------------------------
 	/**
 	 * @return void
 	 */
 	abstract public function registerActions(): void;
+	// -----------------------------------------------------------------------------------------------------------------
+	/**
+	 * @param string $action
+	 * @param callable $fn
+	 * @return void
+	 * @throws BragaException
+	 */
+	protected function registerAction(string $action, callable $fn): void
+	{
+		$this->parentController->registerAction($action, $fn);
+	}
 	// -----------------------------------------------------------------------------------------------------------------
 }
