114808b253d050ec0142d3d4a184e41645053c46
Tomasz Gajewski <tgajewski007@gmail.com>
2025-12-28 15:25:45 +0100

    dodanie wyjątku NotImplementedYetException

---
+++ src/exception/NotImplementedYetException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace braga\tools\exception;
+
+/**
+ * Created 28.12.2025 15:25
+ * error prefix
+ * @autor Tomasz Gajewski
+ */
+class NotImplementedYetException extends BragaException
+{
+}
